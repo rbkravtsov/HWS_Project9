642e29741f11478eb53babec87af9ed0d9e47ba6
rbkravtsov <rkravtsov@me.com>
2019-01-15 23:06:25 +0300

    Add files via upload
    The same project as HWS_Project7, but with GCD

---
+++ HWS_Project9/HWS_Project9/Petition.swift
@@ -0,0 +1,17 @@
+//
+//  Petition.swift
+//  HWS_Project7
+//
+//  Created by Roman Kravtsov on 13/01/2019.
+//  Copyright © 2019 Roman Kravtsov. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+
---
+++ HWS_Project9/HWS_Project9/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  HWS_Project7
+//
+//  Created by Roman Kravtsov on 13/01/2019.
+//  Copyright © 2019 Roman Kravtsov. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
